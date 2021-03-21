7241f6394624c77590be0b16ac794e4fd577f986
Nick <nhsilas12@gmail.com>
2021-03-20 21:45:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -12,18 +12,49 @@ client = discord.Client(intents=intents)
 @client.event
 async def on_ready():
 
+    # print(
+    #     f'{client.user} has connected to the following servers:\n'
+    #     f'{guild.name}(id: {guild.id})'
+    # )
+
+    # members = '\n - '.join([member.name for member in guild.members])
+    # print(f'Guild Members:\n - {members}')
+
+
+    game = discord.Game("with the API")
+    await client.change_presence(activity=game)
+
+    channelName = 123
     for guild in client.guilds:
         if guild == GUILD:
+         break
+
+
+
+
+    for channel in guild.channels:
+        print(f'{channel}')
+        if channel.name == "general":
+            print(f'{channel}\n')
+            channelName = channel.id
+            print(f'{channelName}\n')
             break
 
-    print(
-        f'{client.user} has connected to the following servers:\n'
-        f'{guild.name}(id: {guild.id})'
-    )
 
-    members = '\n - '.join([member.name for member in guild.members])
-    print(f'Guild Members:\n - {members}')
 
+
+
+@client.event
+async def on_message(message):
+
+    if message.channel.id == 773595120369008650:
+        print(f'{message.author}')
+        print(f'{message.content}')
+
+    channel = client.get_channel(773595120369008650)
+    printMessage = input()
+    await channel.send(printMessage)
+    
 
 
 
